dbebe94b42336d156df5605e674411075f99a0d3
kuanr <kuanrasulkhan@gmail.com>
2024-02-29 15:32:11 +0500

    Dodi

---
+++ app/src/main/java/com/example/apecapplication/Library.kt
@@ -0,0 +1,19 @@
+package com.example.apecapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Library : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page123) // Устанавливаем разметку для активности
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/apecapplication/RegistrationPage.kt
+++ app/src/main/java/com/example/apecapplication/RegistrationPage.kt
@@ -1,22 +1,26 @@
-package com.example.apecapplication
-
-import android.annotation.SuppressLint
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-class RegistrationPage : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.page12) // Устанавливаем разметку для активности
-
-//        val button: Button = findViewById(R.id.startbutton)
-//        button.setOnClickListener {
-//            val intent = Intent(this, Main::class.java)
-//            startActivity(intent)
-//        }
-
-
-
-    }
-}
+//package com.example.apecapplication
+//
+//import DishesActivity
+//import android.annotation.SuppressLint
+//import android.content.Intent
+//import android.os.Bundle
+//import android.view.View
+//import android.widget.Button
+//import androidx.appcompat.app.AppCompatActivity
+//
+//class RegistrationPage : AppCompatActivity() {
+//    @SuppressLint("MissingInflatedId")
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.page12) // Устанавливаем разметку для активности
+//
+////        val button: Button = findViewById(R.id.startbutton)
+////        button.setOnClickListener {
+////            val intent = Intent(this, Main::class.java)
+////            startActivity(intent)
+////        }
+//
+//
+//
+//    }
+//}
